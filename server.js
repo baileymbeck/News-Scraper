3dc808828e291c319e3e6f24f0457b21936a1bc2
Bailey Beck <baileymbeck@gmail.com>
2019-10-28 20:32:21 -0500

    mongodb uri

--- server.js
+++ server.js
@@ -26,8 +26,12 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
+
+mongoose.connect(MONGODB_URI);
+
+
 
 // Routes
 
